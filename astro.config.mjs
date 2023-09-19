41954a423f79d1dde483ff8e1f1875ea1d475777
Barry Jacobs <barry.jacobs@mechenet.com>
2023-09-19 04:36:26 +0100

    Removed test code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https:www.myblog-astro.co.uk",
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap()],
 });
